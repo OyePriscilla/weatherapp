337afb8254b2d504b1b8f52728b7fe05cbd9eeae
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-09-02 11:47:33 +0100

    Add style

--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,20 @@ import { NavLink } from 'react-router-dom';
 function Header() {
   return (
     <div style={{
-      display: 'flex', alignItems: 'center', padding: '0 20px', justifyContent: 'space-between', border: '3px solid rgb(206, 197, 197)', width: '100%',
+      display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0 20px', border: '1px solid rgb(206, 197, 197)', width: '95%', margin: '0 auto',
     }}
     >
       <nav>
-        <NavLink to="/" style={{ fontSize: '48px', color: '#fff', marginLeft: '30px' }}>
-          <i className="fa fa-angle-left" />
+        <NavLink to="/" style={{ fontSize: '38px', color: '#fff', marginLeft: '10px' }}>
+          <i className="fa fa-angle-left" style={{ width: '20%' }} />
         </NavLink>
       </nav>
-      <h2 style={{ fontSize: '28px', color: '#fff' }}>Weather App</h2>
+      <h2 style={{ fontSize: '18px', color: '#fff', width: '3%' }}>WEATHER</h2>
       <div style={{
-        fontSize: '28px', color: '#fff', marginLeft: '130px', marginRight: '150px',
+        fontSize: '18px', color: '#fff', marginLeft: '130px', marginRight: '150px',
       }}
       >
-        <i className="fa fa-microphone" style={{ marginRight: '10px' }} />
+        <i className="fa fa-microphone" style={{ marginRight: '2px' }} />
         <i className="fa fa-cog" />
       </div>
     </div>
